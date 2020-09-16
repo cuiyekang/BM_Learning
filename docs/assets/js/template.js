13033254d7128e67ef9b12e7f6716a8b5e4e9f28
cuiyekang <cyk1007@163.com>
2020-09-16 17:32:17 +0800

    .

--- docs/assets/js/template.js
+++ docs/assets/js/template.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
-
+	$(".navbar").load("in_navbar.html");
+	$("#footer").load("in_footer.html");
 	$(".headroom").headroom({
 		"tolerance": 20,
 		"offset": 50,
@@ -10,4 +11,5 @@ jQuery(document).ready(function($) {
 		}
 	});
 
+	
 });
